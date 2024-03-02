9d90f450f42fe139018c918f45389f533ab37fea
sonjaJolanda <80689952+sonjaJolanda@users.noreply.github.com>
2024-03-02 15:57:52 +0100

    init - basic functionality there but not working the way i want it to

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="bootstrap.css" />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+      integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1>Merkliste</h1>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Beschreibung</th>
+                <th scope="col">Datum</th>
+                <th scope="col">Uhrzeit</th>
+                <th scope="col">Erledigt</th>
+                <th scope="col">Aktion</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <?php 
+               
+                  require_once 'notes.php';
+                  $notes = Note::get_notes();
+
+                  while($row = mysqli_fetch_assoc($notes)){
+                    ?>
+                      <td> <?php echo $row['Name']; ?> </td>
+                      <td> <?php echo $row['Beschreibung']; ?> </td>
+                      <td> <?php echo $row['Datum']; ?> </td>
+                      <td> <?php echo $row['Uhrzeit']; ?> </td>
+                      <td> <?php echo $row['Status']; ?> </td>
+                      <!-- <td><input type="checkbox" /></td> -->
+                      <!-- Aktion: <span class="fa fa-edit mr-1"></span><span class="fa fa-times"></span> -->
+                    </tr>
+                    <?php 
+                  }
+                ?>
+            </tbody>
+          </table>
+          <hr />
+          <h2>Neue Notiz anlegen</h2>
+          <form method="post" action="
+                <?php 
+                  require_once 'notes.php'; 
+                  $note = new Note();
+                  $note->post_note();
+                ?>">
+            <div class="row">
+              <div class="col-12">
+                <label>Name</label>
+                <input class="form-control" id="name" name="name"/>
+              </div>
+              <div class="col-12">
+                <label>Beschreibung</label>
+                <textarea class="form-control" id="description" name="description"></textarea>
+              </div>
+              <div class="col-6">
+                <label>Datum</label>
+                <input class="form-control" id="date" name="date"/>
+              </div>
+              <div class="col-6">
+                <label>Uhrzeit</label>
+                <input class="form-control" id="time" name="time"/>
+              </div>
+              <div class="col-12">
+                <br />
+                <button class="btn btn-success">Anlegen</button>
+                <button class="btn btn-danger">Zurücksetzen</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ notes.php
@@ -0,0 +1,60 @@
+<?php
+
+// object orientation
+// change primary key from name to another id
+// make get and post in same format 
+// insertion of time and date doesnt work
+// throw exceptions
+// nach einfügen von  neuer notiz die liste neu laden damit sie aktualisiert wird
+
+$servername = "localhost"; 
+    $username = "root"; 
+    $password = ""; 
+    $dbname = "merkliste_db"; 
+
+    $connection =  new mysqli($servername, $username, $password, $dbname); 
+    if ($connection->connect_error) { 
+        die("Connection failed: " . $connection->connect_error); 
+    }
+
+class Note {
+    private $name;
+    private $description;
+    private $date;
+    private $time;
+    
+    function __construct() {
+        $this->name = isset($_POST['name']) ? $_POST['name'] : null;
+        $this->description = isset($_POST['description']) ? $_POST['description'] : null;
+        $this->date = isset($_POST['date']) ? $_POST['date'] : null;
+        $this->time = isset($_POST['time']) ? $_POST['time'] : null;
+        //$this->state = $_POST['status']; //do 
+    }
+
+    public function post_note(){
+
+        global $connection;
+
+        $post_query = 'insert into notizen(Name, Beschreibung) values(?, ?)';
+        //$insertion = $connection->prepare("insert into notizen(Name, Beschreibung, Datum, Uhrzeit) values(?, ?, ?, ?)");
+        $post_stmt = $connection->prepare($post_query);
+        $post_stmt->bind_param("ss", $this->name, $this->description); 
+        //$insertion->bind_param("ssii", $this->name, $this->description, $this->date, $this->time); //state
+        
+        $post_stmt->execute() or trigger_error($post_stmt->error, E_USER_ERROR);
+        echo "insertion of ". $this->name ." note successful";
+        $post_stmt->close();
+        $connection->close();
+
+    }
+    public static function get_notes(){
+        global $connection;
+        $get_query = 'select * from notizen';
+        $result = mysqli_query($connection, $get_query);
+        return $result;
+    }
+  
+}
+
+    
+?>
